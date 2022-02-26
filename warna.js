03a4c542907227c8a944d3780d59e28df3bb75bd
robicahyadi26 <obycuby@gmail.com>
2022-02-26 14:56:54 +0700

    merubah warna.html

--- warna.js
+++ warna.js
@@ -33,7 +33,7 @@ const sMerah = document.querySelector('input[name=sMerah]');
 const sHijau = document.querySelector('input[name=sHijau]');
 const sBiru = document.querySelector('input[name=sBiru]');
 
-sMerah.addEventListener('change', function(){
+sMerah.addEventListener('input', function(){
     //console.log( sMerah.value);
     const r = sMerah.value;
     const g = sHijau.value;
@@ -41,7 +41,7 @@ sMerah.addEventListener('change', function(){
     document.body.style.backgroundColor = 'rgb( '+ r +','+ g +','+ b +')';
 });
 
-sHijau.addEventListener('change', function(){
+sHijau.addEventListener('input', function(){
     //console.log( sMerah.value);
     const r = sMerah.value;
     const g = sHijau.value;
@@ -50,10 +50,25 @@ sHijau.addEventListener('change', function(){
 });
 
 
-sBiru.addEventListener('change', function(){
+sBiru.addEventListener('input', function(){
     //console.log( sMerah.value);
     const r = sMerah.value;
     const g = sHijau.value;
     const b = sBiru.value;
     document.body.style.backgroundColor = 'rgb( '+ r +','+ g +','+ b +')';
+});
+
+document.body.addEventListener('mousemove', function(event){
+    //posisi mouse 
+    // console.log( e.clientX );
+    // console.log( e.clientY);
+
+    //ukuran browser
+    // console.log(window.innerWidth);
+
+    const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+    // console.log(xPos);
+    const yPos = Math.round((event.clientY / window.innerHeight) * 255);
+    // console.log(yPos);
+    document.body.style.backgroundColor = 'rgb('+ xPos +','+ yPos +',255)';
 });
